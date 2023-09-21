e5f92fbadff5eb6596d529f599016b5d2121ee80
Zack Smith <acidprime@wallcity.org>
2023-09-21 15:12:01 -0600

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"github.com/pteich/hid"
-	"github.com/pteich/usbsymbolreader/scanner"
+	"github.com/acidprime/usbsymbolreader/scanner"
 	"log"
 	"os"
 	"os/signal"
